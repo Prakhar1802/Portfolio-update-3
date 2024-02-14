77173a82dc0683ec60d992a4a56b1908143f366e
Prakhar Tripathi <134915330+Prakhar1802@users.noreply.github.com>
2024-02-14 21:00:58 +0530

    Update style.js

--- style.js
+++ style.js
@@ -2,3 +2,28 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+let value_display = document.querySelectorAll(".num");
+let interval = 3000;
+
+value_display.forEach((value_display) => {
+   let startvalue = 0
+   let endvalue  = parseInt(value_display.getAttribute("data-val"));
+
+   let duration =  Math.floor(interval/endvalue);
+
+   let counter = setInterval(function(){
+    startvalue += 1;
+    value_display.textContent = startvalue;
+
+    if(startvalue == endvalue){
+        clearInterval(counter);
+    }
+   }, duration)
+});
+
+
+// window.addEventListener("scroll", function(){
+//     var header = document.querySelector("header");
+//     header.classList.toggle("sticky", window.scrollY > 0);
+// })
